f006d502a0523520e7661c4eac724560b710506e
senkawa <mao@amatsuka.me>
2023-03-07 17:20:55 +0800

    :shipit:

---
+++ go.mod
@@ -0,0 +1,20 @@
+module dank
+
+go 1.19
+
+require (
+	github.com/AlecAivazis/survey/v2 v2.3.6
+	github.com/fatih/color v1.14.1
+	github.com/lukasjarosch/go-docx v0.4.5-0.20230207135630-ab2f30734eb0
+)
+
+require (
+	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mgutz/ansi v0.0.0-20170206155736-9520e82c474b // indirect
+	golang.org/x/net v0.0.0-20200925080053-05aa5d4ee321 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/term v0.0.0-20210503060354-a79de5458b56 // indirect
+	golang.org/x/text v0.3.3 // indirect
+)
---
+++ main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/AlecAivazis/survey/v2/terminal"
+	"github.com/fatih/color"
+	"github.com/lukasjarosch/go-docx"
+	"log"
+	"os"
+	"time"
+)
+
+type answers struct {
+	Background string
+	Task       string
+	Result     string
+	Feedback   string
+	Week       string
+	Date       string
+}
+
+type questions struct {
+	Name      string
+	Message   string
+	Default   string
+	Multiline bool
+}
+
+var toAsk = []questions{
+	{Name: "Background", Message: "Background? {background}", Default: "N/A", Multiline: true},
+	{Name: "Task", Message: "Task? {task}", Default: "N/A", Multiline: true},
+	{Name: "Result", Message: "Result? {result}", Default: "N/A", Multiline: true},
+	{Name: "Feedback", Message: "Feedback? {feedback}", Default: "N/A", Multiline: true},
+	{Name: "Week", Message: "Week? {week} (Defaults to current ISO week)", Default: getWeekOfTheYear()},
+	{Name: "Date", Message: "Date? {date} (Defaults to current date)", Default: time.Now().Format("2006-01-02")},
+}
+
+func getWeekOfTheYear() string {
+	_, week := time.Now().ISOWeek()
+	return fmt.Sprintf("%d", week)
+}
+
+func parseQuestions() []*survey.Question {
+	var qs []*survey.Question
+	for _, q := range toAsk {
+		if q.Multiline {
+			qs = append(qs, &survey.Question{
+				Name:   q.Name,
+				Prompt: &survey.Multiline{Message: q.Message, Default: q.Default},
+			})
+			continue
+		}
+
+		qs = append(qs, &survey.Question{
+			Name:   q.Name,
+			Prompt: &survey.Input{Message: q.Message, Default: q.Default},
+		})
+	}
+	return qs
+}
+
+var reportTypes = []string{"IWSP", "Capstone"}
+
+const help = `
+Prepare a "template.docx" in the current directory with the following placeholders: {background}, {task}, {result}, {feedback}, {week}, {date}
+If you're lazy, you can use the same contents for both report types.
+
+- Leave any of the placeholders empty for N/A. For background/task/result/feedback, multiple lines are supported.
+- Leave {date} empty for the current date.
+- Leave {week} empty for the current ISO week.
+- Outputs will follow the naming convention: {type}-{week}.docx
+
+`
+
+func main() {
+	fmt.Print(help)
+
+	sameContent := false
+	if err := survey.AskOne(&survey.Confirm{
+		Message: "Use the same content for both report types?",
+		Help:    "If you select no, you'll be asked for the content twice.",
+	}, &sameContent); err != nil {
+		if errors.Is(err, terminal.InterruptErr) {
+			os.Exit(0)
+		}
+	}
+
+	var lookup answers
+	for _, reportType := range reportTypes {
+		if reportType == reportTypes[0] || !sameContent {
+			lookup = askForAnswers(reportType)
+		}
+
+		doc, err := docx.Open("template.docx")
+		if err != nil {
+			log.Fatalf("error opening template: %v", err)
+		}
+
+		if err = doc.ReplaceAll(docx.PlaceholderMap{
+			"background": lookup.Background,
+			"task":       lookup.Task,
+			"result":     lookup.Result,
+			"feedback":   lookup.Feedback,
+			"week":       lookup.Week,
+			"date":       lookup.Date,
+			"type":       reportType,
+		}); err != nil {
+			log.Fatal(err)
+		}
+
+		filename := fmt.Sprintf("output/%s-%s.docx", reportType, lookup.Week)
+		if err = doc.WriteToFile(filename); err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("Created %s.\n", color.HiYellowString(filename))
+	}
+}
+
+func askForAnswers(reportType string) answers {
+	fmt.Printf("%s Filling in report type: %s\n\n", color.YellowString("#"), reportType)
+
+	qs := parseQuestions()
+
+	var lookup answers
+	if err := survey.Ask(qs, &lookup); err != nil {
+		if errors.Is(err, terminal.InterruptErr) {
+			os.Exit(0)
+		}
+	}
+
+	return lookup
+}
